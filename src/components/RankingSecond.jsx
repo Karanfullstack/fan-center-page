531bd9d77a4e625e1d01d0368c7d71ede1bc129c
Karan Chauhan <karanchouhan211@gmail.com>
2025-08-09 17:08:07 +0200

    add fix in ranking2 complete

--- src/components/RankingSecond.jsx
+++ src/components/RankingSecond.jsx
@@ -4,20 +4,20 @@ export default function RankingSecond() {
             <figure className="h-full flex w-full lg:flex-row lg:space-x-6   lg:space-y-0   space-y-9 flex-col items-start justify-center">
                 <section
                     className="
-                text-base space-y-5.5 lg:w-1/2 md:w-full  flex items-start flex-col justify-center h-auto w-full
+                text-base space-y-5.5 lg:w-1/2    md:w-full flex items-start flex-col justify-center h-auto w-full
                 "
                 >
                     <h2
                         className="
                         
-                    font-bold leading-[1.5] text-[clamp(28px,2vw,30px)]
+                    font-bold leading-[1.5] text-[clamp(28px,1vw,30px)]
                     
                     "
                     >
                         Ranking kolejki
                     </h2>
                     <div
-                        className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday h-[116px] lg:h-[64px]  
+                        className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday min-h-[116px]  lg:min-h-[64px]  
                         
                      "
                     >
@@ -44,20 +44,20 @@ export default function RankingSecond() {
                 </section>
 
                 {/* section second */}
-                <section className="text-base lg:w-1/2 md:w-full md:space-y-9 lg:space-y-0 lg:gap-[1.4rem] space-y-3.5 flex items-start flex-col justify-center h-auto  ">
-                    <h2 className="font-bold leading-[1.5] text-[clamp(28px,2vw,29px)]">
+                <section className="text-base lg:w-1/2 md:w-full w-full md:space-y-9 lg:space-y-0 lg:gap-[1.4rem] space-y-3.5 flex items-start flex-col justify-center h-auto  ">
+                    <h2 className="font-bold leading-[1.5] text-[clamp(28px,1vw,29px)]">
                         Ranking klubowy (miesięczny)
                     </h2>
 
                     {[1, 2, 3].map((_item, index) => (
-                        <div className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday h-[116px] lg:h-[64px]  ">
+                        <div className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday min-h-[116px]  lg:min-h-[64px]  ">
                             <div className="flex space-y-1 lg:space-y-0 lg:space-x-1 lg:flex-row lg:items-center  flex-col justify-center items-start">
                                 <span className="text-xl md:text-xl  text-primary font-bold leading-[100%]">
                                     {index + 1}.
                                 </span>
                                 <span
-                                    className="text-[clamp(14px,1.5vw,15px)] leading-[1.2] font-medium text-base
-                            md:text-[clamp(14px,2vw,16px)]"
+                                    className="text-[clamp(14px,1.5vw,15px)] leading-[1.5] font-medium text-base
+                            md:text-[clamp(14px,1vw,16px)]"
                                 >
                                     Bon do wykorzystania w Media Expert
                                     <br />+ Wkład Klubu/Ligi
