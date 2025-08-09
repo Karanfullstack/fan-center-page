d713484c767a53d13c5c706b9b0a29a7a1a17199
Karan Chauhan <karanchouhan211@gmail.com>
2025-08-09 16:46:54 +0200

    add rankingTwo page final

--- src/components/RankingSecond.jsx
+++ src/components/RankingSecond.jsx
@@ -1,24 +1,30 @@
 export default function RankingSecond() {
     return (
-        <article className="flex flex-col space-y-7 items-center font-hubot-semicondensed px-5 pt-10 md:pt-14 container min-h-[100dvh] mx-w-[1400px] w-full mx-auto justify-start">
-            <figure className="h-full flex w-full  space-y-9 flex-col items-start justify-center">
+        <article className="flex flex-col space-y-7  items-center font-hubot-semicondensed px-5 pt-10 md:pt-14 container min-h-[100dvh] mx-w-[1400px] w-full mx-auto justify-start">
+            <figure className="h-full flex w-full lg:flex-row lg:space-x-6   lg:space-y-0   space-y-9 flex-col items-start justify-center">
                 <section
                     className="
-                text-base   space-y-5.5 flex items-start flex-col justify-center h-fit w-full"
+                text-base space-y-5.5 lg:w-1/2 md:w-full  flex items-start flex-col justify-center h-auto w-full
+                "
                 >
-                    <h2 className="font-bold leading-[1.5] text-[clamp(28px,2vw,29px)] md:text-[clamp(42px,4vw,44px)]">
+                    <h2
+                        className="
+                        
+                    font-bold leading-[1.5] text-[clamp(28px,2vw,30px)]
+                    
+                    "
+                    >
                         Ranking kolejki
                     </h2>
                     <div
-                        className="max-w-[min(800px,100%)]  w-full  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed mx-auto bg-seconday h-[116px] md:h-[140px] 
+                        className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday h-[116px] lg:h-[64px]  
                         
                      "
                     >
-                        <div className="flex space-y-1 md:space-y-2  flex-col justify-center items-start">
+                        <div className="flex space-y-1 lg:space-y-0 lg:space-x-1 lg:flex-row lg:items-center  flex-col justify-center items-start">
                             <span
                                 className="
                             text-xl text-primary font-bold leading-[100%]
-                            md:text-2xl
                             
                             "
                             >
@@ -27,39 +33,37 @@ export default function RankingSecond() {
                             <span
                                 className="
                             text-[clamp(14px,1.5vw,15px)] leading-[1.5] font-medium text-base
-                            md:text-[clamp(20px,2vw,22px)]
+                           
                             "
                             >
                                 Bon do wykorzystania w Media Expert
                             </span>
                         </div>
-                        <span className="font-bold   text-2xl md:text-3xl leding-[1.5]">
-                            500 zł
-                        </span>
+                        <span className="font-bold text-2xl leding-[1.5]">500 zł</span>
                     </div>
                 </section>
 
-                <section className="text-base md:space-y-9  space-y-3.5 flex items-start flex-col justify-center h-fit w-full  ">
-                    <h2 className="font-bold leading-[1.5] text-[clamp(28px,2vw,29px)] md:text-[clamp(42px,4vw,44px)]">
+                {/* section second */}
+                <section className="text-base lg:w-1/2 md:w-full md:space-y-9 lg:space-y-0 lg:gap-[1.4rem] space-y-3.5 flex items-start flex-col justify-center h-auto  ">
+                    <h2 className="font-bold leading-[1.5] text-[clamp(28px,2vw,29px)]">
                         Ranking klubowy (miesięczny)
                     </h2>
 
                     {[1, 2, 3].map((_item, index) => (
-                        <div className="max-w-[min(800px,100%)] md:space-y-2 space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday h-[116px] md:h-[140px] ">
-                            <div className="flex space-y-1  flex-col justify-center items-start">
-                                <span className="text-xl md:text-2xl  text-primary font-bold leading-[100%]">
+                        <div className="max-w-[min(800px,100%)] lg:flex-row lg:justify-between lg:items-center md:space-y-2 lg:space-y-0  space-y-1 p-4 flex flex-col justify-start items-start font-hubot-semicondensed w-full mx-auto bg-seconday h-[116px] lg:h-[64px]  ">
+                            <div className="flex space-y-1 lg:space-y-0 lg:space-x-1 lg:flex-row lg:items-center  flex-col justify-center items-start">
+                                <span className="text-xl md:text-xl  text-primary font-bold leading-[100%]">
                                     {index + 1}.
                                 </span>
                                 <span
-                                    className="text-[clamp(14px,1.5vw,15px)] leading-[1.5] font-medium text-base
-                            md:text-[clamp(20px,2vw,22px)]"
+                                    className="text-[clamp(14px,1.5vw,15px)] leading-[1.2] font-medium text-base
+                            md:text-[clamp(14px,2vw,16px)]"
                                 >
                                     Bon do wykorzystania w Media Expert
+                                    <br />+ Wkład Klubu/Ligi
                                 </span>
                             </div>
-                            <span className="font-bold  text-2xl md:text-3xl leding-[1.5]">
-                                500 zł
-                            </span>
+                            <span className="font-bold text-2xl  leding-[1.5]">500 zł</span>
                         </div>
                     ))}
                 </section>
