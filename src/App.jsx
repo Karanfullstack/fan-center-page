e9efd9f31212adf3bc8f723c94c753be170cb7a6
Karan Chauhan <karanchouhan211@gmail.com>
2025-08-09 17:39:34 +0200

    add smallcard component final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import RankingSecond from './components/RankingSecond';
+import SlidersRanking from './components/SlidersRanking';
 
 export default function App() {
     return (
-        <main className="container h-[100dvh] max-w-[1440px] mx-auto w-full">
+        <main className="container  h-[100dvh] max-w-[1440px] mx-auto w-full">
             <Hero />
             <RankingSecond />
+            <SlidersRanking />
         </main>
     );
 }
---
+++ src/components/SlidersRanking.jsx
@@ -0,0 +1,13 @@
+export default function SlidersRanking() {
+    return (
+        <figure className="w-[160px] p-4  flex flex-col justify-center items-start bg-seconday h-[160px] gap-3 aspect-square text-base  ">
+            <span className=" text-primary text-xl font-bold leading-1">1.</span>
+            <div className="flex flex-col items-start justify-center">
+                <span className="text-2xl font-bold leading-[1.5]">500 zł</span>
+                <span className="text-sm leading-[1.5] break-words font-medium">
+                    Bon do wykorzystania w Media Expert
+                </span>
+            </div>
+        </figure>
+    );
+}
